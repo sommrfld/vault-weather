88c796c80c611b02f619383e6f19dfbe36d413b0
Phillip Sommerfeld <phil.sommerfeld@gmail.com>
2014-03-22 16:37:16 -0500

    Empty Project

---
+++ WeatherTest/WeatherTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WeatherTest
+//
+//  Created by Phillip Sommerfeld on 3/22/14.
+//  Copyright (c) 2014 P. Sommerfeld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WeatherTest/WeatherTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WeatherTest
+//
+//  Created by Phillip Sommerfeld on 3/22/14.
+//  Copyright (c) 2014 P. Sommerfeld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
